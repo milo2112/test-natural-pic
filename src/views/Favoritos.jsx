82cae98d3edfdf771b2147ab2f6064c106c84cda
milo2112 <mjrp.ros@gmail.com>
2023-07-13 14:34:15 -0400

    Modifica nombre a prop

--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -1,10 +1,7 @@
-import GlobalContext from "../context/GlobalContext"
-import { useContext } from "react"
 import Galeria from "../components/Galeria";
 
 export default function Favoritos() {
-  const { photos } = useContext(GlobalContext)
-  
+ 
   return (
     <>
       <h1>Fotos favoritas</h1>
@@ -15,14 +12,3 @@ export default function Favoritos() {
     </>  
   );
 }
-
-
-      // <div className="galeria grid-columns-5 p-3">
-      //   {photos.filter(({ liked }) => liked ).map(({ id, src }) => (
-      //     <div 
-      //       key={id}
-      //       className="foto" 
-      //       style={{ backgroundImage: `url(${src.tiny})` }}>
-      //     </div>
-      //   ))}
-      // </div> 
